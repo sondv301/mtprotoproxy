0e5f89ea14f1acd27e13957e0259efb083c18827
sondv301 <81000007+sondv301@users.noreply.github.com>
2025-05-26 16:02:05 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
-    # "tg2": "0123456789abcdef0123456789abcdef",
+    "tg": "dd" + "aed0b1b8ecbf4535660dcfa9d3fd8acb"
 }
 
 MODES = {
